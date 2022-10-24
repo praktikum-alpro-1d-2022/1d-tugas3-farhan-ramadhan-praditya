4dcc50587ffacb57a06e40614873297d1f154414
Praditya <114642975+kleinklain@users.noreply.github.com>
2022-10-24 18:12:37 +0700

    Create soal_1.cpp

---
+++ soal_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main (){
+
+    float alas = 8, tinggi = 10, sisi_miring;
+
+    cout << "Nilai Alas = " << alas << endl;
+    cout << "Nilai tinggi = " << tinggi << endl;
+
+    sisi_miring = sqrt ( pow(alas,2) + pow(tinggi,2) );
+
+    cout << "Nilai sisi miring = " << miring << endl;
+
+    return 0;
+}
